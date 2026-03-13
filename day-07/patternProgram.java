f84ce7d3c39b4c1b416dd7d13ae2e1a0828f2255
Ankith Kumar verma <167072211+ankithverma04@users.noreply.github.com>
2026-03-13 20:28:29 +0530

    Create patternProgram.java

---
+++ day-07/patternProgram.java
@@ -0,0 +1,48 @@
+public class EasyPatternPrograms {
+
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        System.out.println("1. Square Star Pattern");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n2. Right Triangle Star Pattern");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n3. Inverted Right Triangle Pattern");
+        for(int i = n; i >= 1; i--) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n4. Number Triangle Pattern");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n5. Repeated Number Pattern");
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
